a67dd25f4b2aa094a84f6f6c27983b618155a182
prayag jain <prayag.jain.120@gmail.com>
2022-06-01 23:56:46 +0530

    assignment on june 1

---
+++ src/logger/logger.js
@@ -0,0 +1,4 @@
+const log = function welcome() {
+    console.log('Welcome to my application. I am Prayag Jain and a part of FunctionUp Radon cohort')
+}
+module.exports.log = log
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const externalModule = require('./logger')
+const externalModule = require('.././logger/logger')
+const externalModule2 = require('.././util/helper')
+const externalModule3 = require('.././validator/formatter')
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
-    console.log('The current batch is '+externalModule.batch)
     externalModule.log()
+    externalModule2.date()
+    externalModule2.month()
+    externalModule2.info()
+    externalModule3.trim()
+    externalModule3.lower()
+    externalModule3.upper()
+
     res.send('My first ever api!')
 });
 
@@ -23,6 +30,10 @@ router.get('/test-me3', function (req, res) {
 });
 
 router.get('/test-me4', function (req, res) {
+    res.send('My last api!')
+});
+router.get('/test-me5', function (req, res) {
+    externalModule.log2()
     res.send('My last api!')
 });
 
