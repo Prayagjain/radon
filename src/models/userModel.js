3919283560a35eec54eed165485d883df659c6ed
prayag jain <prayag.jain.120@gmail.com>
2022-06-07 04:00:38 +0530

    randon-change

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -7,27 +7,15 @@ const bookSchema = new mongoose.Schema( {
     authorName: {type:String,
                  required: true
                 },
-    category: {
-        type: String,
-        unique: true,
-        required: true
-    },
+    category: {type: String,
+               unique: true,
+               required: true
+              },
     year: {type: Number,
            required: true,
           },
     
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    
 }, { timestamps: true });
 
-module.exports = mongoose.model('Book', bookSchema) //users
-
-
-
-// String, Number
-// Boolean, Object/json, array
+module.exports = mongoose.model('Book', bookSchema) //books
